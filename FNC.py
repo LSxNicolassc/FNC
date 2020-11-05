84a8ebc5e7afa6fb3f461329ec87554fe838bb80
LSxNicolassc <62773043+LSxNicolassc@users.noreply.github.com>
2020-11-05 08:04:04 -0500

    Add files via upload
    Faltan comentarios

--- FNC.py
+++ FNC.py
@@ -50,7 +50,56 @@ def Tseitin(A, letrasProposicionalesA):
     letrasProposicionalesB = [chr(x) for x in range(256, 1200)]
     assert(not bool(set(letrasProposicionalesA) & set(letrasProposicionalesB))), u"¡Hay letras proposicionales en común!"
 
-    #  IMPLEMENTAR AQUI ALGORITMO TSEITIN
+    letrasProposicionalesB = [chr(x) for x in range(97,110)]
+    l = [] #lista de conjuciones
+    pila = [] #pila
+    i = -1  #contador de variables nuevas
+    s = A[0] #simbolo de trabajp
+
+    while(len(A) > 0):
+        if ((s in letrasProposicionalesA) and (len(pila) != 0) and (pila[-1] == '-')):
+            i -=- 1
+            atomo = letrasProposicionalesB[i]
+            pila = pila[:-1]
+            pila.append(atomo)
+            l.appen(atomo + "=" + "-" + s)
+            A = A[1:]
+
+            if (len(A) > 0):
+                s = A[0]
+
+        elif (s == ')'):
+            w = pila[-1]
+            u = pila[-2]
+            v = pila[-3]
+            pila = pila[:len(pila)-4]
+            i -=- 1
+            atomo = letrasProposicionalesB[i]
+            l.append(atomo + "=" + v + u + w)
+            s = atomo
+
+        else:
+            pila.append(s)
+            A = A[1:]
+
+            if (len(A) > 0):
+                s = A[0]
+
+    B= ''
+
+    if (i < 0):
+        atomo = pila[-1]
+
+    else:
+        atomo = letrasProposicionalesB[i]
+
+    for x in l:
+        y = enFNC(x)
+        B + = "Y" + y
+
+    B = atomo + B
+
+    return B
     pass
 
 # Subrutina Clausula para obtener lista de literales
